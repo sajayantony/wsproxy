9c59a2c9f8f57e5298e5a34d5c59c2c352c4bf4b
Sajay Antony <sajaya@microsoft.com>
2017-11-01 17:54:24 -0700

    Initial commit.

--- TTYProxy/Startup.cs
+++ TTYProxy/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TTYProxy.WSProxy;
 
 namespace TTYProxy
 {
@@ -35,6 +38,43 @@ namespace TTYProxy
             }
 
             app.UseMvc();
+            var webSocketOptions = new WebSocketOptions()
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(120),
+                ReceiveBufferSize = 4 * 1024
+            };
+            app.UseWebSockets(webSocketOptions);
+
+
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/ws")
+                {
+                    if (context.WebSockets.IsWebSocketRequest)
+                    {
+                        WebSocket clientWs = await context.WebSockets.AcceptWebSocketAsync();
+                        using (var ws = new ClientWebSocket())
+                        {
+                            await ws.ConnectAsync(new Uri("https://localhost:8080"), 
+                                                CancellationToken.None);
+
+                            if (ws.State == WebSocketState.Open)
+                            {
+                                await ws.BindStreams(clientWs, CancellationToken.None); // Pass in valid token.
+                            }
+                        }
+                    }
+                    else
+                    {
+                        context.Response.StatusCode = 400;
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
         }
     }
 }
---
+++ TTYProxy/WSProxy/WSExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TTYProxy.WSProxy
+{
+    public static class WSExtensions
+    {
+        public static Task BindStreams(this WebSocket socket1, WebSocket socket2, CancellationToken token)
+        {
+            // Copy streams both ways. 
+            // Replace with an ReplaySubject on source. 
+            var t1 = Task.Run(async () => await socket1.CopyAsync(socket2, token));
+            var t2 = Task.Run(async () => await socket2.CopyAsync(socket1, token));
+            return Task.WhenAll(t1, t2);
+        }
+
+        public static async Task CopyAsync(this WebSocket src, WebSocket dst, CancellationToken token)
+        {
+            var buffer = new byte[1024 * 4];
+            while (true)
+            {
+                var result = await src.ReceiveAsync(new ArraySegment<byte>(buffer), token);
+
+                if (!result.CloseStatus.HasValue)
+                {
+                    await dst.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count),
+                                        result.MessageType,
+                                        result.EndOfMessage,
+                                        CancellationToken.None);
+                }
+                else
+                {
+                    await dst.CloseAsync(result.CloseStatus.Value, 
+                                        result.CloseStatusDescription, 
+                                        CancellationToken.None);
+                    break;
+                }       
+            }
+        }
+    }
+}
